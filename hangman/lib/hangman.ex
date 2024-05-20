9745b6288661d6aad0c6eec1a4f27074e0235709
Tomeu <teco@trifork.com>
2024-05-20 20:51:53 +0200

    some typing

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,15 +1,21 @@
 defmodule Hangman do
+  alias Hangman.Impl.Game
+
   @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
-  @type game :: any
+  # opaque - keep ur fingers out of my internal state outside this module
+  @opaque game :: Game.t()
   @type tally :: %{
           turns_left: integer,
           game_state: state,
           letters: list(String.t()),
           used: list(String.t())
         }
+
   @spec new_game() :: game
-  def new_game do
-  end
+  # def new_game do
+  #   Game.new_game()
+  # end
+  defdelegate new_game, to: Game
 
   @spec make_move(game, String.t()) :: {game, tally}
   def make_move(_game, _guess) do
--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,11 +1,17 @@
 defmodule Hangman.Impl.Game do
+  @type t :: %__MODULE__{
+          turns_left: integer,
+          game_state: Hangman.state(),
+          letters: list(String.t()),
+          used: MapSet.t(String.t())
+        }
   defstruct turns_left: 7,
             game_state: :initializing,
             letters: [],
             used: MapSet.new()
 
   def new_game do
-    %Hangman.Impl.Game{
+    %__MODULE__{
       letters: Dictionary.random_word() |> String.codepoints()
     }
   end
