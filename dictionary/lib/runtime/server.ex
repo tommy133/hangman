b6bd6fc918dff38e33bc7efc4807807c91d568e3
Tomeu <teco@trifork.com>
2024-05-31 09:10:33 +0200

    add supervisor

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,15 +1,20 @@
 defmodule Dictionary.Runtime.Server do
   alias Dictionary.Impl.WordList
+  use Agent
 
   @type t :: pid()
 
   @me __MODULE__
 
-  def start_link do
+  def start_link(_) do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
   def random_word() do
+    if :rand.uniform() < 0.33 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
+
     Agent.get(@me, &WordList.random_word/1)
   end
 end
